f9b9aa18ac4f297f2ea557b7e5360b03e634231c
mestn1y <mestn1y@hotmail.com>
2025-07-07 22:35:00 +0300

    added styles for menu

--- src/js/menuBurger.js
+++ src/js/menuBurger.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const burgerBtn = document.querySelector('.header-btn');
-  const closeBtn = document.querySelector('.menu-close');
-  const burgerMenu = document.querySelector('.burger-menu');
+const burgerBtn = document.querySelector('.header-btn');
+const closeBtn = document.querySelector('.menu-close');
+const burgerMenu = document.querySelector('.burger-menu');
 
-  burgerBtn.addEventListener('click', () => {
-    burgerMenu.classList.add('active');
-  });
+burgerBtn.addEventListener('click', () => {
+  burgerMenu.classList.add('active');
+  document.body.classList.add('no-scroll');
+});
 
-  closeBtn.addEventListener('click', () => {
-    burgerMenu.classList.remove('active');
-  });
+closeBtn.addEventListener('click', () => {
+  burgerMenu.classList.remove('active');
+  document.body.classList.remove('no-scroll');
 });
